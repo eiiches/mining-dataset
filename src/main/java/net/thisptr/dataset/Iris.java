ce38314bba957e3b234488fdb3856d175115f4d1
Eiichi Sato <sato.eiichi@gmail.com>
2013-03-26 01:06:51 +0900

    Get rid of mining-core dependency.

--- src/main/java/net/thisptr/dataset/Iris.java
+++ src/main/java/net/thisptr/dataset/Iris.java
@@ -1,16 +1,11 @@
 package net.thisptr.dataset;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import net.thisptr.encoder.bean.annotation.EncodableBeans;
-import net.thisptr.encoder.bean.annotation.EncodeWith;
-import net.thisptr.encoder.bean.attribute.NominalAttributeEncoder;
-import net.thisptr.util.IOEnumerators;
-
+import org.apache.commons.io.IOUtils;
 
 /**
  * Fischer's iris data set.
@@ -23,7 +18,6 @@ public class Iris {
 		Virginica
 	}
 	
-	@EncodableBeans
 	public static class IrisEntry {
 		public double sepalLength;
 		public double sepalWidth;
@@ -31,7 +25,6 @@ public class Iris {
 		public double petalLength;
 		public double petalWidth;
 		
-		@EncodeWith(NominalAttributeEncoder.class)
 		public Species species;
 	}
 	
@@ -39,21 +32,24 @@ public class Iris {
 	
 	public List<IrisEntry> getValues() {
 		final List<IrisEntry> result = new ArrayList<IrisEntry>();
-		try (final InputStream is = getClass().getClassLoader().getResourceAsStream("dataset/iris.dat")) {
-			for (final String line : IOEnumerators.readLines(is).skip(1)) {
+		
+		try {
+			final List<String> lines = IOUtils.readLines(getClass().getClassLoader().getResourceAsStream("dataset/iris.dat"));
+			for (final String line : lines.subList(1, lines.size())) {
 				final String[] values = TAB.split(line);
-				
+	
 				final IrisEntry value = new IrisEntry();
 				value.sepalLength = Double.valueOf(values[0]);
 				value.sepalWidth = Double.valueOf(values[1]);
 				value.petalLength = Double.valueOf(values[2]);
 				value.petalWidth = Double.valueOf(values[3]);
 				value.species = Species.valueOf(values[4]);
-				result.add(value);
+				result.add(value);		
 			}
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+		
 		return result;
 	}
 }
