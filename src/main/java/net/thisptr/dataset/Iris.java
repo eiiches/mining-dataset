707ce119abf6471daa6a1a70a755e300c26c0fbd
Eiichi Sato <sato.eiichi@gmail.com>
2014-02-11 11:08:51 +0900

    Replace commons-io with google-guava.

--- src/main/java/net/thisptr/dataset/Iris.java
+++ src/main/java/net/thisptr/dataset/Iris.java
@@ -1,15 +1,16 @@
 package net.thisptr.dataset;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import org.apache.commons.io.IOUtils;
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
 
 /**
- * Fischer's iris data set.
- * http://en.wikipedia.org/wiki/Iris_flower_data_set
+ * Fischer's iris data set. http://en.wikipedia.org/wiki/Iris_flower_data_set
  */
 public class Iris {
 	public static enum Species {
@@ -17,39 +18,39 @@ public class Iris {
 		Versicolor,
 		Virginica
 	}
-	
+
 	public static class IrisEntry {
 		public double sepalLength;
 		public double sepalWidth;
-		
+
 		public double petalLength;
 		public double petalWidth;
-		
+
 		public Species species;
 	}
-	
+
 	private static final Pattern TAB = Pattern.compile("\t");
-	
+
 	public List<IrisEntry> getValues() {
 		final List<IrisEntry> result = new ArrayList<IrisEntry>();
-		
-		try {
-			final List<String> lines = IOUtils.readLines(getClass().getClassLoader().getResourceAsStream("dataset/iris.dat"));
+
+		try (final InputStreamReader reader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("dataset/iris.dat"), Charsets.UTF_8)) {
+			final List<String> lines = CharStreams.readLines(reader);
 			for (final String line : lines.subList(1, lines.size())) {
 				final String[] values = TAB.split(line);
-	
+
 				final IrisEntry value = new IrisEntry();
 				value.sepalLength = Double.valueOf(values[0]);
 				value.sepalWidth = Double.valueOf(values[1]);
 				value.petalLength = Double.valueOf(values[2]);
 				value.petalWidth = Double.valueOf(values[3]);
 				value.species = Species.valueOf(values[4]);
-				result.add(value);		
+				result.add(value);
 			}
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		return result;
 	}
 }
